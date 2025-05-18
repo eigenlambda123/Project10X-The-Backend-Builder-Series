ea24acc538afaf49d27985ee62094c56071ff687
Rodmark "RM" Villa <rmvilla987@gmail.com>
2025-05-18 13:45:44 +0800

    Post, Tag, and Category registeres to admin

--- personal-blogging-platform-API/blog_app/admin.py
+++ personal-blogging-platform-API/blog_app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Post, Category, Tag
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+    Admin interface for the Post model.
+    """
+    list_display = ('title', 'author', 'category', 'created_at')
+    prepopulated_fields = {'slug': ('title',)}  # Automatically populate the slug field based on the title
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """
+    Admin interface for the Category model.
+    """
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    """
+    Admin interface for the Tag model.
+    """
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
