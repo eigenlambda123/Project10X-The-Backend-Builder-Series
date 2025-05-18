fbfb52e35391967783a166a47850986b684241eb
Rodmark "RM" Villa <rmvilla987@gmail.com>
2025-05-18 13:12:10 +0800

    created a Category model with name and slug field

--- personal-blogging-platform-API/blog_app/models.py
+++ personal-blogging-platform-API/blog_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+# Create Category model
+class Category(models.Model):
+    """
+    Category model to categorize blog posts.
+    """
+    name = models.CharField(max_length=100, unique=True) # Field for category name
+    slug = models.SlugField(max_length=100, unique=True, blank=True) # Slug field for URL-friendly representation
+
+    # Create a slug from the name before saving
+    def save(self, *args, **kwargs):
+        if not self.slug: # Only create a slug if it doesn't already exist
+            # Use slugify to create a URL-friendly slug from the name
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs) # Call the parent class's save method
+
+    # String representation of the model
+    # This will be used when we print the object
+    def __str__(self):
+        return self.name
+
