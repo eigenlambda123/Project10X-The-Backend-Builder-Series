4daf5856a50e0ee16fff2c1f529df3612e53854a
eigenlambda123 <rmvilla987@gmail.com>
2025-05-19 19:02:25 +0800

    Added permission for PostViewSet:  - IsOwnerOrReadOnly (for handling permissions if the current user is the owner of the post or not, if owner then full access, if not then read-only)  - IsAuthenticatedOrReadOnly (for handling permissions if the user is authenticated or not, if authenticated then full access, if not then read-only)

--- personal-blogging-platform-API/blog_app/views.py
+++ personal-blogging-platform-API/blog_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Post
 from .serializers import PostSerializer
+from .permissions import IsOwnerOrReadOnly 
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class PostViewSet(viewsets.ModelViewSet):
     """
@@ -9,6 +11,7 @@ class PostViewSet(viewsets.ModelViewSet):
     """
     queryset = Post.objects.all()  # Queryset to retrieve all posts
     serializer_class = PostSerializer  # Serializer class to use for serialization
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]  # Permissions for the viewset
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)  # Save the post with the current user as the author
