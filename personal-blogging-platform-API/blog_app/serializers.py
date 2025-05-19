32be8fd88d722d54dc74b2e2307a3e5c0643fbf4
eigenlambda123 <rmvilla987@gmail.com>
2025-05-19 18:47:24 +0800

    created PostSerializer for Post model

--- personal-blogging-platform-API/blog_app/serializers.py
+++ personal-blogging-platform-API/blog_app/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Category, Tag
+from .models import Category, Tag, Post
+from markdown import markdown
 
 class CategorySerializer(serializers.ModelSerializer):
     """
@@ -19,4 +20,22 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ['id', 'name', 'slug']  # Include the fields you want to serialize
         read_only_fields = ['id', 'slug'] # Specify which fields are read-only
+        
+
+class PostSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Post model.
+    """
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'slug', 'content', 'html_content', 'created_at', 'updated_at', 'category', 'tags']
+        read_only_fields = ['id', 'slug', 'created_at', 'updated_at']
+
+    def get_html_content(self, obj):
+        """
+        Convert the raw Markdown content of a Post into HTML
+        when serializing it in the API response
+        """
+        return markdown(obj.content)
         
