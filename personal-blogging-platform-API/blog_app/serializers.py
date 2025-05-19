38434c7fc4895e2a9d2675fcbb6761da5f1cc5eb
eigenlambda123 <rmvilla987@gmail.com>
2025-05-19 18:44:19 +0800

    created TagSerializer for Tag model

--- personal-blogging-platform-API/blog_app/serializers.py
+++ personal-blogging-platform-API/blog_app/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Category
+from .models import Category, Tag
 
 class CategorySerializer(serializers.ModelSerializer):
     """
@@ -7,6 +7,16 @@ class CategorySerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Category
+        fields = ['id', 'name', 'slug']  # Include the fields you want to serialize
+        read_only_fields = ['id', 'slug'] # Specify which fields are read-only
+
+
+class TagSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Tag model.
+    """
+    class Meta:
+        model = Tag
         fields = ['id', 'name', 'slug']  # Include the fields you want to serialize
         read_only_fields = ['id', 'slug'] # Specify which fields are read-only
         
