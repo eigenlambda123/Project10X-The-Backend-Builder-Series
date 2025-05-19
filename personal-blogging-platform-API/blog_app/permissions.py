2b7fc18e2d8fcb346d65a9b0d324c0a81a629046
eigenlambda123 <rmvilla987@gmail.com>
2025-05-19 19:01:30 +0800

    created custom IsOwnerOrReadOnly permission

---
+++ personal-blogging-platform-API/blog_app/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions
+
+# Permission for allowing owner to modify their own posts or read-only access for others
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+        
