61d559dab2004e68f54f050466bd844cd9d23279
eigenlambda123 <rmvilla987@gmail.com>
2025-05-20 09:06:40 +0800

    add basic filtering using DjangoFilterBackend

--- personal-blogging-platform-API/blog_app/views.py
+++ personal-blogging-platform-API/blog_app/views.py
@@ -4,6 +4,8 @@ from .models import Post
 from .serializers import PostSerializer
 from .permissions import IsOwnerOrReadOnly 
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class PostViewSet(viewsets.ModelViewSet):
     """
@@ -12,6 +14,8 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()  # Queryset to retrieve all posts
     serializer_class = PostSerializer  # Serializer class to use for serialization
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]  # Permissions for the viewset
+    filter_backends = [DjangoFilterBackend]  # Filter backend for filtering posts
+    filterset_fields = ['category', 'tags']  # Fields to filter by
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)  # Save the post with the current user as the author
