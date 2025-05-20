b56074e0c86a018d8d45ddd9b29de01d3d008636
eigenlambda123 <rmvilla987@gmail.com>
2025-05-20 13:59:22 +0800

    add URL enpoint for RegisterViewSet

--- personal-blogging-platform-API/blog_app/urls.py
+++ personal-blogging-platform-API/blog_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet
+from .views import PostViewSet, RegisterViewSet
 
 # Create a router and register our viewset with it.
 router = DefaultRouter()
@@ -9,4 +9,9 @@ router.register(r'posts', PostViewSet, basename='post')
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
     path('', include(router.urls)),
+]
+
+# Add the registration view to the urlpatterns
+urlpatterns += [
+    path('api/register/', RegisterViewSet.as_view(), name='register'),  # Registration view
 ]
