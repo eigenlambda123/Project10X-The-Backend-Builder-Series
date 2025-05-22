39d7ed4458dca8f63fce2b023bcb9642aada43f8
eigenlambda123 <rmvilla987@gmail.com>
2025-05-22 11:39:18 +0800

    add router endpoints por category and tag views

--- personal-blogging-platform-API/blog_app/urls.py
+++ personal-blogging-platform-API/blog_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, RegisterViewSet, PostDetailBySlugView
+from .views import PostViewSet, RegisterViewSet, PostDetailBySlugView, CategoryViewSet, TagViewSet
 
 # Create a router and register our viewset with it.
 router = DefaultRouter()
 router.register(r'posts', PostViewSet, basename='post')
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'tags', TagViewSet, basename='tag')
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
