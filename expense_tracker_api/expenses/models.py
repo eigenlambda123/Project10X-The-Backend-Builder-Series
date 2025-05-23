d0ef3ac170e01d59d1b99df22fd01a06128a7418
eigenlambda123 <rmvilla987@gmail.com>
2025-05-23 10:51:38 +0800

    added transaction model

--- expense_tracker_api/expenses/models.py
+++ expense_tracker_api/expenses/models.py
@@ -13,3 +13,19 @@ class Category(models.Model):
     def __str__(self): # String representation of the category
         self.name
 
+
+class Transactions(models.Model):
+    TRANSACTION_TYPE = [
+        ('income', 'Income'),
+        ('expense', 'Expense'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions') # User who created the transaction
+    category = models.ForeignKey(Category, on_delete=models.CASCADE) # Category of the transaction
+    title = models.CharField(max_length=100) # Title of the transaction
+    description = models.TextField(blank=True) # Description of the transaction
+    type = models.CharField(max_length=7, choices=TRANSACTION_TYPE) # Type of the transaction (income or expense)
+    amount = models.DecimalField(max_digits=10, decimal_places=2) # Amount of the transaction
+    description = models.TextField(blank=True) # Description of the transaction
+    created_at = models.DateTimeField(auto_now_add=True) # Timestamp when the transaction was created
+    updated_at = models.DateTimeField(auto_now=True) # Timestamp when the transaction was last updated
