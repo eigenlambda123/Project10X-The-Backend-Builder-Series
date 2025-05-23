e17e0d988b34f2d779aff4020db9ce64fa44d67f
eigenlambda123 <rmvilla987@gmail.com>
2025-05-23 13:44:16 +0800

    add

--- expense_tracker_api/expenses/views.py
+++ expense_tracker_api/expenses/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from . serializers import TransactionsSerializer, CategorySerializer
 from . models import Transactions, Category
+from . permissions import IsOwnerOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all() # Get all categories
     serializer_class = CategorySerializer # Use the CategorySerializer for serialization
-    permission_classes = [IsAuthenticated] # Only authenticated users can access this view
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly] # Only authenticated users can access this view
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user) # Filter categories to only those owned by the current user
@@ -15,7 +16,7 @@ class CategoryViewSet(ModelViewSet):
 class TransactionsViewSet(ModelViewSet):
     queryset = Transactions.objects.all() # Get all transactions
     serializer_class = TransactionsSerializer # Use the TransactionsSerializer for serialization
-    permission_classes = [IsAuthenticated] # Only authenticated users can access this view
+    permission_classes = [IsAuthenticated, IsOwnerOrReadOnly] # Only authenticated users can access this view
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user) # Filter transactions to only those owned by the current user
