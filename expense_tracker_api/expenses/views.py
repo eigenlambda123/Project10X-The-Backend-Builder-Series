3bf5eef9ea06f41c04a77d20944cc042d95ed5ed
eigenlambda123 <rmvilla987@gmail.com>
2025-05-23 11:57:25 +0800

    add CategoryViewSet using ModelViewSet

--- expense_tracker_api/expenses/views.py
+++ expense_tracker_api/expenses/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from . serializers import TransactionsSerializer, CategorySerializer
+from . models import Transactions, Category
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all() # Get all categories
+    serializer_class = CategorySerializer # Use the CategorySerializer for serialization
+    permission_classes = [IsAuthenticated] # Only authenticated users can access this view
+
+    def get_queryset(self):
+        return self.queryset.filter(user=self.request.user) # Filter categories to only those owned by the current user
+    
+    
