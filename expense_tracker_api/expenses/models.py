e5a3ed6ffc0fe2ef58825c8d82585fb0b42e78aa
eigenlambda123 <rmvilla987@gmail.com>
2025-05-23 10:47:09 +0800

    add category model

--- expense_tracker_api/expenses/models.py
+++ expense_tracker_api/expenses/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100) # Name of the category
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # User who created the category
+    slug = models.SlugField(max_length=100, unique=True) # Slug for the category
+
+    class Meta:
+        unique_together = ('name', 'user') # Ensure that the combination of name and user is unique
+        ordering = ['name'] # Order categories by name
+
+    def __str__(self): # String representation of the category
+        self.name
+
