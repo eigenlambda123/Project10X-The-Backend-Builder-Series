fd10a44c6c07feba30713e2d917616fc503e0a53
eigenlambda123 <rmvilla987@gmail.com>
2025-05-23 10:55:41 +0800

    register Transactions model to admin

--- expense_tracker_api/expenses/admin.py
+++ expense_tracker_api/expenses/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import Transactions, Category
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', 'user', 'created_at')
-    list_filter = ('user',)
-    search_fields = ('name',)
+@admin.register(Category) # Register the Category model with the admin site
+class CategoryAdmin(admin.ModelAdmin): 
+    list_display = ('name', 'user', 'created_at') # Display these fields in the admin list view
+    list_filter = ('user',) # Filter by user in the admin list view
+    search_fields = ('name',) # Search by name in the admin list view
+
+@admin.register(Transactions)
+class TransactionsAdmin(admin.ModelAdmin): # Register the Transactions model with the admin site
+    list_display = ('user', 'title', 'amount', 'type', 'category','date', 'created_at')
+    list_filter = ('user', 'type', 'category', 'date') # Filter by user, type, category, and date in the admin list view
+    search_fields = ('title', 'description')
