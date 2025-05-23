d61647c1c5bd1c4a2792f43a9139f98b983c8e5f
eigenlambda123 <rmvilla987@gmail.com>
2025-05-23 11:18:22 +0800

    add read_only fields

--- expense_tracker_api/expenses/serializers.py
+++ expense_tracker_api/expenses/serializers.py
@@ -6,10 +6,12 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'user', 'slug', 'created_at'] # Fields to be serialized
+        read_only_fields = ['user', 'slug', 'created_at'] # Fields that are read-only
 
 
 class TransactionsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transactions
         fields = ['id', 'user', 'category', 'title', 'description', 'type', 'amount', 'created_at', 'updated_at'] # Fields to be serialized
+        read_only_fields = ['user', 'created_at', 'updated_at']
 
