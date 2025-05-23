a2c0933ff2cf7195ab3e80949b2339d094a3fcad
eigenlambda123 <rmvilla987@gmail.com>
2025-05-23 10:59:20 +0800

    remove date in transaction admin

--- expense_tracker_api/expenses/admin.py
+++ expense_tracker_api/expenses/admin.py
@@ -10,5 +10,5 @@ class CategoryAdmin(admin.ModelAdmin):
 @admin.register(Transactions)
 class TransactionsAdmin(admin.ModelAdmin): # Register the Transactions model with the admin site
     list_display = ('user', 'title', 'amount', 'type', 'category', 'created_at')
-    list_filter = ('user', 'type', 'category', 'date') # Filter by user, type, category, and date in the admin list view
+    list_filter = ('user', 'type', 'category') # Filter by user, type, category, and date in the admin list view
     search_fields = ('title', 'description')
