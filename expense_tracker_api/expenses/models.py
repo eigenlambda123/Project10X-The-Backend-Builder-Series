4a68a0830074e9eff63181ef3f855f1c35b981b1
eigenlambda123 <rmvilla987@gmail.com>
2025-05-23 10:57:21 +0800

    add created_at in Category model

--- expense_tracker_api/expenses/models.py
+++ expense_tracker_api/expenses/models.py
@@ -5,6 +5,7 @@ class Category(models.Model):
     name = models.CharField(max_length=100) # Name of the category
     user = models.ForeignKey(User, on_delete=models.CASCADE) # User who created the category
     slug = models.SlugField(max_length=100, unique=True) # Slug for the category
+    created_at = models.DateTimeField(auto_now_add=True) # Timestamp when the category was created
 
     class Meta:
         unique_together = ('name', 'user') # Ensure that the combination of name and user is unique
