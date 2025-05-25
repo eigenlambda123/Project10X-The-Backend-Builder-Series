4a7d170f9e8d052fdcc91ec14f5123514218ab48
eigenlambda123 <rmvilla987@gmail.com>
2025-05-25 11:49:50 +0800

    add RegisterSerializer for handling user registration

--- expense_tracker_api/expenses/serializers.py
+++ expense_tracker_api/expenses/serializers.py
@@ -2,6 +2,27 @@ from .models import Transactions, Category
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)  # Password field that is write-only
+
+    class Meta:
+        model = User # User model to be serialized
+        fields = ['username', 'email', 'password'] # Fields to be serialized
+
+    # Create a new user instance with the validated data
+    def create(self, validated_data):
+        # Create a new user with the provided username, email, and password
+        if User.objects.filter(username=validated_data['username']).exists(): # Check if the username already exists
+            raise serializers.ValidationError({"username": "Username already exists."})
+        # If the username is unique, create the user
+        user = User.objects.create_user( 
+            username=validated_data['username'], 
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        return user
+
+
 class CategorySerializer(serializers.ModelSerializer):
     user = serializers.HiddenField(default=serializers.CurrentUserDefault()) # Automatically set the user to the currently authenticated user
 
