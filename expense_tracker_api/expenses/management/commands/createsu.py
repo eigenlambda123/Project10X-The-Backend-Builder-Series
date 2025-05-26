d07acc8161bead1e7e3da328c0cd41f0ef65bcc5
eigenlambda123 <rmvilla987@gmail.com>
2025-05-26 09:14:50 +0800

    made a script the will automatically create superuser on render

---
+++ expense_tracker_api/expenses/management/commands/createsu.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Create a superuser with the specified username and password'
+
+    def handle(self, *args, **kwargs):
+        """
+        Handle the command to create a superuser.
+        """
+        User = get_user_model()
+        if not User.objects.filter(is_superuser=True).exists():
+            User.objects.create_superuser(
+                username='kiyo',
+                email='admin@example.com',
+                password='adminpass123'
+            )
+            self.stdout.write(self.style.SUCCESS('Superuser created.'))
+        else:
+            self.stdout.write(self.style.WARNING('Superuser already exists.'))
