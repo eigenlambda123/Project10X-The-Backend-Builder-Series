e26eafd17b958d0dedc964e08805d36e596940ea
eigenlambda123 <rmvilla987@gmail.com>
2025-05-27 13:47:40 +0800

    Initialize rest_framework settings

--- url_shortener_service/url_shortener/settings.py
+++ url_shortener_service/url_shortener/settings.py
@@ -125,3 +125,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    # JWT setup (later phase)
+    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
+    # ),
+}
