5cf52f562a1ef3ec9268d8e10f77326f23593b65
eigenlambda123 <rmvilla987@gmail.com>
2025-05-27 14:08:49 +0800

    created a generate_short_code function for generating short code

--- url_shortener_service/shortener/models.py
+++ url_shortener_service/shortener/models.py
@@ -3,6 +3,11 @@ from django.contrib.auth.models import User
 import string
 import random
 
+def generate_short_code(length=6):
+    """Generate a random short code of specified length."""
+    characters = string.ascii_letters + string.digits  # Characters to use in the short code
+    return ''.join(random.choice(characters) for _ in range(length))  # Generate the short code
+
 class ShortURL(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) # Optional foreign key to User model
     original_url = models.URLField()  # Field to store the original URL
