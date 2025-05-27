24a3a1a779f600976ce5d6727b1a87c90e4ed410
eigenlambda123 <rmvilla987@gmail.com>
2025-05-27 14:17:22 +0800

    registered ShortURL and ClickEvent to admin

--- url_shortener_service/shortener/admin.py
+++ url_shortener_service/shortener/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import ShortURL, ClickEvent
 
-# Register your models here.
+@admin.register(ShortURL)
+class ShortURLAdmin(admin.ModelAdmin):
+    list_display = ('short_code', 'original_url', 'clicks', 'created_at', 'is_active', 'expiration_date', 'user')
+    search_fields = ('short_code', 'original_url')
+    list_filter = ('is_active',)
+
+@admin.register(ClickEvent)
+class ClickEventAdmin(admin.ModelAdmin):
+    list_display = ('short_url', 'clicked_at', 'ip_address')
+    search_fields = ('short_url__short_code', 'ip_address')
+
