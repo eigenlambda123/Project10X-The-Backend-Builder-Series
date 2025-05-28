79fdec8d914b581999100a68f4b12ec28dde1c1d
eigenlambda123 <rmvilla987@gmail.com>
2025-05-28 11:12:05 +0800

    implement hashids for generating shortcodes intead of random strings and numbers

--- url_shortener_service/shortener/models.py
+++ url_shortener_service/shortener/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 import string
 import random
+from hashids import Hashids
 
-def generate_short_code(length=6):
-    """Generate a random short code of specified length."""
-    characters = string.ascii_letters + string.digits  # Characters to use in the short code
-    return ''.join(random.choice(characters) for _ in range(length))  # Generate the short code
+# def generate_short_code(length=6):
+#     """Generate a random short code of specified length."""
+#     characters = string.ascii_letters + string.digits  # Characters to use in the short code
+#     return ''.join(random.choice(characters) for _ in range(length))  # Generate the short code
+
+hashids = Hashids(min_length=6)  # Initialize Hashids for generating short codes
 
 class ShortURL(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True) # Optional foreign key to User model
@@ -18,13 +21,17 @@ class ShortURL(models.Model):
     is_active = models.BooleanField(default=True) # Field to indicate if the short URL is active
     
     def save(self, *args, **kwargs):
-        """Override save method to generate short code if not provided."""
-        if not self.short_code: # If short_code is not set, generate a new one
-            self.short_code = generate_short_code()
-            while ShortURL.objects.filter(short_code=self.short_code).exists(): # Ensure the generated short code is unique
-                self.short_code = generate_short_code()
-        super().save(*args, **kwargs) # save the model instance
-
+        """Override the save method to generate a short code if not already set."""
+        # If the object is new (no id), save it first to get an id
+        if not self.id:
+           super().save(*args, **kwargs)
+        # If the short_code is not set, generate it using Hashids
+        if not self.short_code:  
+            self.short_code = hashids.encode(self.id)
+            super().save(update_fields=['short_code'])
+        # If the short_code is already set, just save the object
+        else:
+            super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.short_code} → {self.original_url}"
