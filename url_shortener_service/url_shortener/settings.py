5ab02ff2bb2c9937c2470e844d41e3407fa6118e
eigenlambda123 <rmvilla987@gmail.com>
2025-05-28 10:12:10 +0800

    set pagination page to 5

--- url_shortener_service/url_shortener/settings.py
+++ url_shortener_service/url_shortener/settings.py
@@ -130,7 +130,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,  # number of items per page
+    'PAGE_SIZE': 5,  # number of items per page
     # JWT setup (later phase)
     # 'DEFAULT_AUTHENTICATION_CLASSES': (
     #     'rest_framework_simplejwt.authentication.JWTAuthentication',
