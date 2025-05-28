014adafeecc684a268e09f5efd54f72a79fc1037
eigenlambda123 <rmvilla987@gmail.com>
2025-05-28 09:18:43 +0800

    Implement redirect view for short URLs with expiration and click tracking     - Added RedirectToOriginalView APIView to handle redirection from short code to original URL.     - Checks if the short URL is active and not expired before redirecting.     - Increments click count and logs each click event with IP address and user agent.     - Returns HTTP 410 if the URL has expired.

--- url_shortener_service/shortener/views.py
+++ url_shortener_service/shortener/views.py
@@ -2,9 +2,19 @@
 from rest_framework import permissions, viewsets
 from . models import ShortURL, ClickEvent
 from . serializers import ShortURLSerializer
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404, redirect
+from django.utils import timezone
+from rest_framework import status
+from rest_framework.response import Response
+
 
 
 class ShortURLViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet for handling ShortURL objects.
+    Provides CRUD operations for ShortURL objects.
+    """
     queryset = ShortURL.objects.all()  # retrieve all ShortURL objects
     serializer_class = ShortURLSerializer  # serialization and deserialization of ShortURL objects
     permission_classes = [permissions.AllowAny]  # Allow any user to access this viewset
@@ -13,3 +23,23 @@ class ShortURLViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user if self.request.user.is_authenticated else None)  # Save the user if authenticated, otherwise None
 
 
+class RedirectToOriginalView(APIView):
+    """"
+    APIView to handle redirection from short code to original URL.
+    """
+    def get(self, request, short_code): # Handle GET requests to redirect to the original URL
+        url_obj = get_object_or_404(ShortURL, short_code=short_code, is_active=True)  # Retrieve the ShortURL object by short_code
+
+        # check if the URL has expired
+        if url_obj.expiration_date and url_obj.expiration_date < timezone.now():
+            return Response({'detail': 'URL has expired.'}, status=status.HTTP_410_GONE)
+        
+        # log the click event
+        url_obj.clicks += 1 # Increment the click count
+        url_obj.save() # Save the updated ShortURL object
+        ClickEvent.objects.create(
+            short_url=url_obj,  # Create a ClickEvent object with the ShortURL object
+            ip_address=request.META.get('REMOTE_ADDR', ''),  # Get the user's IP address
+            user_agent=request.META.get('HTTP_USER_AGENT', '')  # Get the user's user agent
+        )
+        return redirect(url_obj.original_url) # Redirect to the original URL
