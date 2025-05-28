f507b36c7aa90c25074d09aaae79e09dbf072c54
eigenlambda123 <rmvilla987@gmail.com>
2025-05-28 09:27:13 +0800

    add custom validation for the url

--- url_shortener_service/shortener/serializers.py
+++ url_shortener_service/shortener/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import ShortURL
+from urllib.parse import urlparse
 
 class ShortURLSerializer(serializers.ModelSerializer):
     short_code = serializers.ReadOnlyField()  # Make short_code read-only since it's generated automatically
@@ -9,3 +10,12 @@ class ShortURLSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShortURL # Define the model to serialize
         fields = ['id', 'user', 'original_url', 'short_code', 'clicks', 'created_at', 'expiration_date', 'is_active'] # Specify the fields to include in the serialized output
+
+    def validate_original_url(self, value):
+        """
+        Validate that the original URL is a valid URL.
+        """
+        parsed = urlparse(value) # Parse the URL to check its validity
+        if not parsed.scheme or not parsed.netloc: # Check if the URL has a valid scheme (http or https) and netloc (domain)
+            raise serializers.ValidationError("Enter a valid URL with scheme (http:// or https://)")
+        return value
