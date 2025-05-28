c928f43d8516826fdc658ba6be556ad4ceea6384
eigenlambda123 <rmvilla987@gmail.com>
2025-05-28 09:37:32 +0800

    edited the Endpoints to use router in ShortURLViewSet

--- url_shortener_service/shortener/urls.py
+++ url_shortener_service/shortener/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from .views import ShortURLViewSet, RedirectToOriginalView, ListUserURLsView
 
+from rest_framework.routers import DefaultRouter
+from shortener.views import ShortURLViewSet
+
+router = DefaultRouter()
+router.register(r'api/shorten', ShortURLViewSet, basename='shorturl')
+
 urlpatterns = [
-    path('api/shorten/', ShortURLViewSet.as_view(), name='shorten-url'),
-    path('api/urls/', ListUserURLsView.as_view(), name='list-urls'),
+    path('api/user-urls/', ListUserURLsView.as_view(), name='user_urls'), 
     path('r/<str:short_code>/', RedirectToOriginalView.as_view(), name='redirect'),
 ]
+
+urlpatterns += router.urls  
