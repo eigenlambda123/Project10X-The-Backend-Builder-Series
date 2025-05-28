1b6a1d74f42d4c9ca25ae6a446d257f3ac2056ee
eigenlambda123 <rmvilla987@gmail.com>
2025-05-28 09:11:00 +0800

    created a ShortURLViewSet

--- url_shortener_service/shortener/views.py
+++ url_shortener_service/shortener/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import permissions, viewsets
+from . models import ShortURL, ClickEvent
+from . serializers import ShortURLSerializer
+
+
+class ShortURLViewSet(viewsets.ModelViewSet):
+    queryset = ShortURL.objects.all()  # retrieve all ShortURL objects
+    serializer_class = ShortURLSerializer  # serialization and deserialization of ShortURL objects
+    permission_classes = [permissions.AllowAny]  # Allow any user to access this viewset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user if self.request.user.is_authenticated else None)  # Save the user if authenticated, otherwise None
+
+
