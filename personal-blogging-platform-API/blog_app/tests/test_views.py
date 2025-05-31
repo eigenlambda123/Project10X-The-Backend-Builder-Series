4588d636acaebcb03ec06c0d45e5cbc0f8813600
eigenlambda123 <rmvilla987@gmail.com>
2025-05-31 11:19:12 +0800

    Add setup for post filtering tests with multiple categories and tags

--- personal-blogging-platform-API/blog_app/tests/test_views.py
+++ personal-blogging-platform-API/blog_app/tests/test_views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from blog_app.models import Post, Category, Tag
 from django.urls import reverse
 from rest_framework import status
+from django.contrib.auth import get_user_model
 
 class PostTestAPI(APITestCase):
     """
@@ -75,3 +76,63 @@ class PostTestAPI(APITestCase):
         response = self.client.delete(self.detail_url) # delete the post via detail_url
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT) # check if the response status is 204 no content
         self.assertFalse(Post.objects.filter(slug=self.post.slug).exists()) # make sure the slug of the delete post doesnt exist
+
+
+
+
+
+User = get_user_model()
+
+class PostFilteringTest(APITestCase):
+    """
+    Sets up test data for verifying post filtering by category and tags.
+    Creates multiple posts with different categories and tags for filter tests.
+    """
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(username="testuser", password="pass1234") # create new user
+        self.client.force_authenticate(user=self.user) # force authenticate user
+
+
+        # create categories
+        self.cat1 = Category.objects.create(name="Tech")
+        self.cat2 = Category.objects.create(name="Life")
+
+        # create tags
+        self.tag1 = Tag.objects.create(name="Django")
+        self.tag2 = Tag.objects.create(name="React")
+
+        # create Posts
+
+        # post1
+        self.post1 = Post.objects.create(
+            title="Post One",
+            content="...",
+            author=self.user,
+            category=self.cat1
+        )
+        self.post1.tags.set([self.tag1]) # add tag1
+
+        # post2
+        self.post2 = Post.objects.create(
+            title="Post Two", 
+            content="...", 
+            author=self.user, 
+            category=self.cat2
+        )
+        self.post2.tags.set([self.tag2]) # add tag2
+
+        # post3
+        self.post3 = Post.objects.create(
+            title="Post Three", 
+            content="...", 
+            author=self.user, 
+            category=self.cat1
+        )
+        self.post3.tags.set([self.tag1, self.tag2])  # add tag1 and tag2
+
+        self.url = reverse("post-list") # use post-list as self.url
+
+
+    
+
