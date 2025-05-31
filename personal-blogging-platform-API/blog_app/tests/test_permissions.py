18243d86eae2e8c2c0935fc1aea0fd9802e42f2b
eigenlambda123 <rmvilla987@gmail.com>
2025-05-31 09:30:32 +0800

    Add test setup for unauthenticated post access permissions

---
+++ personal-blogging-platform-API/blog_app/tests/test_permissions.py
@@ -0,0 +1,27 @@
+from rest_framework.test import APITestCase, APIClient
+from django.contrib.auth.models import User
+from blog_app.models import Post, Category, Tag
+from django.urls import reverse
+
+class PostUnauthenticatedAccessTest(APITestCase):
+    """
+    Tests that unauthenticated users can read posts
+    but cannot create, update, or delete them.
+    """
+    def setUp(self):
+        self.user = User.objects.create_user(username="owner", password="secret") # create a user
+        self.category = Category.objects.create(name="Django") # create new category
+        self.tag = Tag.objects.create(name="test") # create a new tag
+
+        # create a new post
+        self.post = Post.objects.create( 
+            title = "Public post",
+            content = "Anyone can read this.",
+            author = self.user,
+            category = self.category
+        )
+        self.post.tags.add(self.tag) # add tag to the post
+        self.client = APIClient
+        self.create_url = reverse('post-list')  # /api/posts/
+        self.detail_url = reverse('post-detail', kwargs={'slug': self.post.slug})  # /api/posts/<slug>/
+
