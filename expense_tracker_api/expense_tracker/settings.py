61ec1196535963da79f071b1f91d9a8aa29ad5ca
eigenlambda123 <rmvilla987@gmail.com>
2025-06-01 09:16:31 +0800

    change database settings for testing

--- expense_tracker_api/expense_tracker/settings.py
+++ expense_tracker_api/expense_tracker/settings.py
@@ -80,9 +80,10 @@ WSGI_APPLICATION = 'expense_tracker.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR}/db.sqlite3'
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
