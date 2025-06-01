937ff42d1803f261c7c33dc59e83af41016ddae5
eigenlambda123 <rmvilla987@gmail.com>
2025-06-01 09:32:30 +0800

    created a test that check if slug will auto generate

--- expense_tracker_api/expenses/tests/test_models.py
+++ expense_tracker_api/expenses/tests/test_models.py
@@ -1,9 +1,20 @@
-from django import Testcase
+from django.test import TestCase
 from django.contrib.auth.models import User
 from expenses.models import Category
 
-class CategoryModelTests(Testcase):
+class CategoryModelTests(TestCase):
     """
     """
     def setUp(self):
         self.user = User.objects.create_user(username='testuser', password='password') # create dummy user
+
+    
+    def test_create_category_and_slug_generation(self):
+        """
+        Test that creating a Category automatically generates the correct slug
+        and that the string representation returns the category name.
+        """
+        category = Category.objects.create(name='Groceries', user=self.user)  # create new category
+        self.assertEqual(category.name, 'Groceries')  # check if the category name is correct
+        self.assertEqual(category.slug, 'groceries')  # check if slug is auto-generated correctly
+        self.assertEqual(str(category), 'Groceries')  # check string representation of category
