7dc5a9eaec11c7ba08ff5cad4cc8d44ed06ceba5
eigenlambda123 <rmvilla987@gmail.com>
2025-06-02 11:52:54 +0800

    created a Test that an authenticated user can access the category and transaction endpoints

--- expense_tracker_api/expenses/tests/test_auth_permissions.py
+++ expense_tracker_api/expenses/tests/test_auth_permissions.py
@@ -4,6 +4,17 @@ from rest_framework import status
 from django.contrib.auth.models import User
 
 class AuthPermissionTests(APITestCase):
+    def authenticate(self):
+        """
+        defined for getting a token via given user
+        """
+        self.user = User.objects.create_user(username='authuser', password='testpass123')
+        url = reverse('token_obtain_pair')
+        response = self.client.post(url, {'username': 'authuser', 'password': 'testpass123'})
+        token = response.data['access']
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
+
+
     def test_user_can_register_successfully(self):
         url = reverse('register') # access register url endpoint
 
@@ -50,4 +61,23 @@ class AuthPermissionTests(APITestCase):
 
         self.assertEqual(response1.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(response2.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+    def test_authorized_access_returns_200(self):
+        """
+        Test that an authenticated user can access the category and transaction endpoints
+        """
+        self.authenticate()  # Authenticate the test client with a valid JWT token
+
+        category_url = reverse('category-list')  # URL for the category-list endpoint
+        expense_url = reverse('transactions-list')  # URL for the transactions-list endpoint
+
+        # Send GET requests to both endpoints as an authenticated user
+        response1 = self.client.get(category_url)
+        response2 = self.client.get(expense_url)
+
+        # Assert that both endpoints return HTTP 200 OK for authenticated access
+        self.assertEqual(response1.status_code, status.HTTP_200_OK)
+        self.assertEqual(response2.status_code, status.HTTP_200_OK)
+
 
