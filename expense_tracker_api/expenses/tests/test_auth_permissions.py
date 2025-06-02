2111dd85bcdc9d96ff5823a672db29177063c7c5
eigenlambda123 <rmvilla987@gmail.com>
2025-06-02 11:40:54 +0800

    created a test for Test that a user can obtain a JWT token using valid credentials

--- expense_tracker_api/expenses/tests/test_auth_permissions.py
+++ expense_tracker_api/expenses/tests/test_auth_permissions.py
@@ -17,3 +17,22 @@ class AuthPermissionTests(APITestCase):
         response = self.client.post(url, data) # access register with APICLient and post the data
         self.assertEqual(response.status_code, status.HTTP_201_CREATED) # check if status 201 created
         self.assertTrue(User.objects.filter(username='testuser').exists()) # check if the user is created successfully
+
+
+    def test_user_can_obtain_jwt_token(self):
+        """
+        Test that a user can obtain a JWT token using valid credentials.
+        """
+        User.objects.create_user(username='testuser', password='strongpassword123')  # Create a new user
+        url = reverse('token_obtain_pair')  # Get the URL for the JWT token endpoint
+
+        # Input user credentials
+        data = {
+            'username': 'testuser',
+            'password': 'strongpassword123'
+        }
+
+        response = self.client.post(url, data)  # Send POST request to obtain JWT token
+        self.assertEqual(response.status_code, status.HTTP_200_OK)  # Check for 200 OK status
+        self.assertIn('access', response.data)  # Check if access token is present in the response
+        self.assertIn('refresh', response.data)  # Check if refresh token is present in the response
