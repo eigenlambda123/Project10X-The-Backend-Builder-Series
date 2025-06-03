c58cf204645b1ea8113bef1e359f3a0e6b844571
eigenlambda123 <rmvilla987@gmail.com>
2025-06-03 09:25:25 +0800

    create a Test if creating a short url is working with the required fields

--- url_shortener_service/shortener/tests/test_models.py
+++ url_shortener_service/shortener/tests/test_models.py
@@ -10,3 +10,16 @@ class ShortURLModelTest(TestCase):
     def setUp(self):
         self.valid_url = "https://example.com"
         self.expiration_date = timezone.now() + timedelta(days=1)
+
+    def test_create_shorturl_with_required_fields(self):
+        """
+        Test if creating a short url is working with the required fields
+        """
+
+        # create a short url
+        url = ShortURL.objects.create(
+            original_url=self.valid_url
+        )
+        self.assertEqual(url.original_url, self.valid_url) # check if the original_url is correctly using valid_url as its value
+        self.assertTrue(url.short_code) # check if short_code is created
+        self.assertEqual(url.clicks, 0) # check how many clicks, should be 0
