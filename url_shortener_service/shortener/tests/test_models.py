b06f3c7d9d0a742581013e89c342db313455ece1
eigenlambda123 <rmvilla987@gmail.com>
2025-06-03 09:27:38 +0800

    created a Test if the date the short url is created is auto generated

--- url_shortener_service/shortener/tests/test_models.py
+++ url_shortener_service/shortener/tests/test_models.py
@@ -23,3 +23,16 @@ class ShortURLModelTest(TestCase):
         self.assertEqual(url.original_url, self.valid_url) # check if the original_url is correctly using valid_url as its value
         self.assertTrue(url.short_code) # check if short_code is created
         self.assertEqual(url.clicks, 0) # check how many clicks, should be 0
+
+
+    def test_created_at_is_auto_generated(self):
+        """
+        Test if the date the short url is created is auto generated
+        """
+
+        # create short url
+        url = ShortURL.objects.create(
+            original_url=self.valid_url
+        )
+        self.assertIsNotNone(url.created_at) # check if created_at is created
+        self.assertAlmostEqual(url.created_at.timestamp(), timezone.now().timestamp(), delta=2)
