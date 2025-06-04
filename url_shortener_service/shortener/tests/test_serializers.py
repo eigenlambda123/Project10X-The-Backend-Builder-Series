899b5eb21ed5e72a55636b6c523d88677a21fe92
eigenlambda123 <rmvilla987@gmail.com>
2025-06-04 09:08:48 +0800

    created a Test if invalid url is rejected

--- url_shortener_service/shortener/tests/test_serializers.py
+++ url_shortener_service/shortener/tests/test_serializers.py
@@ -18,3 +18,12 @@ class ShortURLSerializerTest(TestCase):
         """
         serializer = ShortURLSerializer(data={"original_url": self.valid_url}) # serialize dummy url
         self.assertTrue(serializer.is_valid(), serializer.errors) # check if dummy url is valid
+
+    def test_invalid_url_is_rejected(self):
+        """
+        Test if invalid url is rejected
+        """
+        invalid_url = "not-a-valid-url" # invalid dummy url
+        serializer = ShortURLSerializer(data={"original_url": invalid_url}) # serialize invalid url
+        self.assertFalse(serializer.is_valid()) # check if not valid
+        self.assertIn("original_url", serializer.errors) # check if the error involves original_url
