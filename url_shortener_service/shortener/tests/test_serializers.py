35a52c5367631f43ad63f8675c295c672ac8a2f6
eigenlambda123 <rmvilla987@gmail.com>
2025-06-04 09:07:19 +0800

    created a Test for accepting valid url

--- url_shortener_service/shortener/tests/test_serializers.py
+++ url_shortener_service/shortener/tests/test_serializers.py
@@ -11,3 +11,10 @@ class ShortURLSerializerTest(TestCase):
         self.valid_url = "https://example.com" # dummy url
         self.future_date = timezone.now() + timedelta(days=1) 
         self.past_date = timezone.now() - timedelta(days=1) 
+
+    def test_valid_original_url_is_accepted(self):
+        """
+        Test for accepting valid url
+        """
+        serializer = ShortURLSerializer(data={"original_url": self.valid_url}) # serialize dummy url
+        self.assertTrue(serializer.is_valid(), serializer.errors) # check if dummy url is valid
