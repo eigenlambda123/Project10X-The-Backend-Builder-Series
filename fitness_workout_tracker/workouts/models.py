442643a1f752f3f412269f8de18ec5010e85604b
eigenlambda123 <rmvilla987@gmail.com>
2025-06-06 09:51:18 +0800

    edited notes field in Workout model

--- fitness_workout_tracker/workouts/models.py
+++ fitness_workout_tracker/workouts/models.py
@@ -8,7 +8,7 @@ class Workout(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE) # Link to user who the workout belongs to
     name = models.CharField(max_length=100) # name of workout
     date = models.DateField() # date of workout
-    notes = models.TextField(blank=True, optional=True) # additional notes about the workout
+    notes = models.TextField(blank=True, null=True) # additional notes about the workout
     created_at = models.DateTimeField(auto_now_add=True) # when the workout was created
     updated_at = models.DateTimeField(auto_now=True) # when the workout was last updated
 
