b794326e8cc976a09a3cd5472b1368daa6483959
eigenlambda123 <rmvilla987@gmail.com>
2025-06-06 11:48:36 +0800

    nested SetSerializer with WorkoutSerializer, WorkoutSerializer should include SetSerializer in its field

--- fitness_workout_tracker/workouts/serializers.py
+++ fitness_workout_tracker/workouts/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Workout, Exercise, Set
 
 class WorkoutSerializer(ModelSerializer):
@@ -13,7 +14,9 @@ class ExerciseSerializer(ModelSerializer):
         fields = ['id', 'category', 'name', 'description']
 
 class SetSerializer(ModelSerializer):
+    exercise_name = serializers.ReadOnlyField(source='exercise.name') # This will return the name of the exercise in the serialized output
+
     class Meta:
         model = Set
-        fields = ['id', 'workout', 'exercise', 'reps', 'weight', 'duration', 'notes', 'created_at', 'updated_at', 'order']
+        fields = ['id', 'workout', 'exercise' ,'exercise_name ', 'reps', 'weight', 'duration', 'notes', 'created_at', 'updated_at', 'order']
         read_only_fields = ['id','created_at', 'updated_at']
