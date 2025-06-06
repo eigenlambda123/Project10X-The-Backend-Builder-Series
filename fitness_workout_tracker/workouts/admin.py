0664e961fdfca6931ddb56eca25e22884e6227ae
eigenlambda123 <rmvilla987@gmail.com>
2025-06-06 10:30:03 +0800

    register Workout, Exercise, and Set model to admin

--- fitness_workout_tracker/workouts/admin.py
+++ fitness_workout_tracker/workouts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Workout, Exercise, Set
 
-# Register your models here.
+@admin.register(Workout)
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'date', 'created_at')
+    search_fields = ('name', 'user__username')
+    list_filter = ('date',)
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+
+@admin.register(Set)
+class SetAdmin(admin.ModelAdmin):
+    list_display = ('workout', 'exercise', 'reps', 'weight', 'order')
+    list_filter = ('workout', 'exercise')
