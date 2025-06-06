af20017240031fa5d66021c7605a9cae4f41bc58
eigenlambda123 <rmvilla987@gmail.com>
2025-06-06 09:46:06 +0800

    created a workout model

--- fitness_workout_tracker/workouts/models.py
+++ fitness_workout_tracker/workouts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Workout(models.Model):
+    """
+    Model representing a workout session for a user
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # Link to user who the workout belongs to
+    name = models.CharField(max_length=100) # name of workout
+    date = models.DateField() # date of workout
+    notes = models.TextField(blank=True, optional=True) # additional notes about the workout
+    created_at = models.DateTimeField(auto_now_add=True) # when the workout was created
+    updated_at = models.DateTimeField(auto_now=True) # when the workout was last updated
+
+    def __str__(self):
+        """
+        String representation of the Workout model
+        """
+        return f"{self.name} - {self.user.username} ({self.date})"
+    
