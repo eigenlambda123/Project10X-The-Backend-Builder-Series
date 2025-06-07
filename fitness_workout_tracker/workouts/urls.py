f44675c207d2299086c0c64759ec8ee815bd064c
eigenlambda123 <rmvilla987@gmail.com>
2025-06-07 11:34:16 +0800

    register ExerciseViewSet to router

--- fitness_workout_tracker/workouts/urls.py
+++ fitness_workout_tracker/workouts/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import WorkoutViewSet, SetViewSet
+from .views import WorkoutViewSet, SetViewSet, ExerciseViewSet
 
 router = DefaultRouter()
 router.register(r'workouts', WorkoutViewSet)
+router.register(r'exercises', ExerciseViewSet, basename='exercise')
 router.register(r'sets', SetViewSet, basename='set')
 
 urlpatterns = [
