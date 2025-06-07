c14619a4c5a58dd2f4bb9eba4af819d3caca9849
eigenlambda123 <rmvilla987@gmail.com>
2025-06-07 12:12:08 +0800

    created a Test creating a new workout via the API endpoint

--- fitness_workout_tracker/workouts/tests/test_api_endpoints.py
+++ fitness_workout_tracker/workouts/tests/test_api_endpoints.py
@@ -1,6 +1,7 @@
 from workouts.views import WorkoutViewSet
 from django.test import TestCase
 from rest_framework.test import APIClient
+from rest_framework import status
 
 
 class WorkoutEndpointTests(TestCase):
@@ -14,6 +15,14 @@ class WorkoutEndpointTests(TestCase):
             "date": "2023-10-01",
             "notes": "This is a test workout."
         }
+
+    def test_create_workout(self):
+        """
+        Test creating a new workout via the API endpoint
+        """
+        response = self.client.post(self.url, self.test_workout_data, format='json') # send POST request to create a new workout
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED) # check if status 201 CREATED
+        self.assertEqual(response.data['name'], self.test_workout_data['name']) # check if the name matches the test data
 
     
 
