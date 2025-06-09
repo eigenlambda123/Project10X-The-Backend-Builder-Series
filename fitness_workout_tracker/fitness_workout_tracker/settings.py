d5425d94e4aa66e1fdd8d70f1d00c9b04bc42bb1
eigenlambda123 <rmvilla987@gmail.com>
2025-06-09 09:54:59 +0800

    implement simpleJWT authentication

--- fitness_workout_tracker/fitness_workout_tracker/settings.py
+++ fitness_workout_tracker/fitness_workout_tracker/settings.py
@@ -134,7 +134,4 @@ REST_FRAMEWORK = {
 		'rest_framework.authentication.SessionAuthentication',
 		'rest_framework_simplejwt.authentication.JWTAuthentication',
 	],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
 }
