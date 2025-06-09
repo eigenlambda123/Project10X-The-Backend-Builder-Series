3b85e0f2280ac5fdcabb4edc8e65600780dd5c6b
eigenlambda123 <rmvilla987@gmail.com>
2025-06-09 10:24:27 +0800

    add endpoint for PersonalRecordsView

--- fitness_workout_tracker/workouts/urls.py
+++ fitness_workout_tracker/workouts/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import WorkoutViewSet, SetViewSet, ExerciseViewSet
+from .views import WorkoutViewSet, SetViewSet, ExerciseViewSet, PersonalRecordsView
 
 router = DefaultRouter()
 router.register(r'workouts', WorkoutViewSet)
@@ -9,4 +9,5 @@ router.register(r'sets', SetViewSet, basename='set')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('analytics/personal-records/', PersonalRecordsView.as_view(), name='personal-records'),
 ]
