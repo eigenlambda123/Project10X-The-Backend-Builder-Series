e5f4d0e9b7de0dc0dde038111cac506ab3df09c2
eigenlambda123 <rmvilla987@gmail.com>
2025-06-09 10:03:59 +0800

    added RegisterView to project url

--- fitness_workout_tracker/fitness_workout_tracker/urls.py
+++ fitness_workout_tracker/fitness_workout_tracker/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from workouts.views import RegisterView
 
 
 urlpatterns = [
@@ -24,4 +25,5 @@ urlpatterns = [
     path('api/', include('workouts.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/register/', RegisterView.as_view(), name='register')
 ]
