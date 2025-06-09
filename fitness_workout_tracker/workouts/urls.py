7b3f04a0af60185ae3ed341e5e61c6eb9bdf94e5
eigenlambda123 <rmvilla987@gmail.com>
2025-06-09 10:33:41 +0800

    added PersonalRecordsView back and add endpoint for WorkoutStreakView

--- fitness_workout_tracker/workouts/urls.py
+++ fitness_workout_tracker/workouts/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from .views import WorkoutViewSet, SetViewSet, ExerciseViewSet, PersonalRecordsView
+from .views import WorkoutViewSet, SetViewSet, ExerciseViewSet, PersonalRecordsView ,WorkoutStreakView
 
 router = DefaultRouter()
 router.register(r'workouts', WorkoutViewSet)
@@ -10,4 +10,5 @@ router.register(r'sets', SetViewSet, basename='set')
 urlpatterns = [
     path('', include(router.urls)),
     path('analytics/personal-records/', PersonalRecordsView.as_view(), name='personal-records'),
+    path('analytics/streaks/', WorkoutStreakView.as_view(), name='workout-streaks'),
 ]
