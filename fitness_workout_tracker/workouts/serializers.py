ce537d5ee16847bf8d4fb5a30ea3af7ecd0c2f54
eigenlambda123 <rmvilla987@gmail.com>
2025-06-09 09:57:19 +0800

    created RegisterSerializer

--- fitness_workout_tracker/workouts/serializers.py
+++ fitness_workout_tracker/workouts/serializers.py
@@ -1,6 +1,22 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework import serializers
 from .models import Workout, Exercise, Set
+from django.contrib.auth.models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password']
+        )
+        return user
 
 class SetSerializer(ModelSerializer):
     exercise_name = serializers.ReadOnlyField(source='exercise.name') # This will return the name of the exercise in the serialized output
